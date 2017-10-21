b781cfe984cb5113fb73eca26f39186ee6206819
juarezeppy <jejuarez88@gmail.com>
2017-10-20 22:39:49 -0700

    Implemented a Select Form
    The form now uses the change event bind to call a method which in turn updates the chart path and data.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,12 +16,14 @@ export class DashboardComponent implements OnInit {
   attendees: Observable<any[]>;
   eventChoices: Observable<any[]>;
   counter: number;
+  isEventSelected: boolean;
 
 
   constructor(private db: AngularFireDatabase, private eventService: EventsService,
               private authService: AuthService, private chartData: ChartDataService) {
     this.eventTitle = 'Select An Event';
     this.counter = 0;
+    this.isEventSelected = false;
   }
 
   ngOnInit() {
@@ -30,20 +32,25 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  changeEvent() {
-    console.log('getting event:', this.eventTitle);
-    this.attendees = this.eventService.getEventData(this.eventTitle.toLowerCase());
+  changeEvent(selectedValue) {
+    console.log('getting event:', selectedValue.value);
 
-    // use a map to manipulate data to return the number of childen to the char service
-    this.db.object('eventAttendees/SoK6mveajVSH6rKCYYa09XABsqD3/' + this.eventTitle.toLowerCase())
-      .snapshotChanges()
-      .map(action => {
-        const size = action.payload.numChildren();
-        return size;
-    }).subscribe(newSize => {
-      this.chartData.changeSize(newSize);
-      this.chartData.changeCheckIns(this.eventTitle.toLowerCase());
-    });
+    if (this.eventTitle !== '') {
+      this.eventTitle = selectedValue.value;
+      this.attendees = this.eventService.getEventData(this.eventTitle.toLowerCase());
+
+      // use a map to manipulate data to return the number of childen to the char service
+      this.db.object('eventAttendees/SoK6mveajVSH6rKCYYa09XABsqD3/' + this.eventTitle.toLowerCase())
+        .snapshotChanges()
+        .map(action => {
+          const size = action.payload.numChildren();
+          return size;
+        }).subscribe(newSize => {
+        this.chartData.changeSize(newSize);
+        this.chartData.changeCheckIns(this.eventTitle.toLowerCase());
+        this.isEventSelected = true;                                  // <-- fix
+      });
+    }
   }
 
 }
